fc70ea716280e3c5a820b56fe4ecafb933a87d2a
jiaqi0524 <113975144+jiaqi0524@users.noreply.github.com>
2022-09-21 10:51:46 +0800

    add html
    add html

---
+++ data/module/8d34b452d72fd265e6862526c87bf55a-readme.php
@@ -0,0 +1,1 @@
+<p style='line-height:150%'><p style='line-height:150%'><p style='line-height:150%'><p style='line-height:150%'>更多模板和插件下载 www.lanrenzhijia.com</p></p></p></p>
---
+++ data/module/modulescache.php
@@ -0,0 +1,7 @@
+<?php
+global $allmodules;
+$GLOBALS['allmodules']['1f35620fb42d452fa2bdc1dee1690f92']='1f35620fb42d452fa2bdc1dee1690f92.xml';
+$GLOBALS['allmodules']['72ffa6fabe3c236f9238a2b281bc0f93']='72ffa6fabe3c236f9238a2b281bc0f93.xml';
+$GLOBALS['allmodules']['8d34b452d72fd265e6862526c87bf55a']='8d34b452d72fd265e6862526c87bf55a.xml';
+$GLOBALS['allmodules']['b437d85a7a7bc778c9c79b5ec36ab9aa']='b437d85a7a7bc778c9c79b5ec36ab9aa.xml';
+?>
